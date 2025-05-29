5f52c5b7b4f86cef71a534045de7f9a842d744de
trinity-1686a <trinity@deuxfleurs.fr>
2025-05-29 21:20:11 +0200

    remove useless changes

--- src/api/s3/error.rs
+++ src/api/s3/error.rs
@@ -87,10 +87,6 @@ pub enum Error {
 	/// The client sent a request for an action not supported by garage
 	#[error(display = "Unimplemented action: {}", _0)]
 	NotImplemented(String),
-
-	/// An internal, usually transient, error happened
-	#[error(display = "Please try again.")]
-	Internal,
 }
 
 commonErrorDerivative!(Error);
@@ -151,7 +147,6 @@ impl Error {
 			Error::InvalidDigest(_) => "InvalidDigest",
 			Error::InvalidUtf8Str(_) | Error::InvalidUtf8String(_) => "InvalidRequest",
 			Error::InvalidEncryptionAlgorithm(_) => "InvalidEncryptionAlgorithmError",
-			Error::Internal => "InternalError",
 		}
 	}
 }
@@ -174,7 +169,6 @@ impl ApiError for Error {
 			| Error::InvalidXml(_)
 			| Error::InvalidUtf8Str(_)
 			| Error::InvalidUtf8String(_) => StatusCode::BAD_REQUEST,
-			Error::Internal => StatusCode::INTERNAL_SERVER_ERROR,
 		}
 	}
 
