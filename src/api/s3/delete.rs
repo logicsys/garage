b0b7606ff7d979cec6e22d77609e53b78daf68db
bytechunk <bytechunk.a52b055@track-it.pw>
2026-02-07 10:58:35 +0000

    S3 api DeleteObject fix invalid XML
    linked to #1323
    
    Do only check element nodes when validating XML content (skip text
    nodes).
    
    If text nodes are skipped then the validation fails when providing
    formatted XML content as body of the request.

--- src/api/s3/delete.rs
+++ src/api/s3/delete.rs
@@ -132,6 +132,11 @@ fn parse_delete_objects_xml(xml: &roxmltree::Document) -> Option<DeleteRequest> 
 	}
 
 	for item in delete.children() {
+		// Only parse <Part> nodes
+		if !item.is_element() {
+			continue;
+		}
+
 		if item.has_tag_name("Object") {
 			let key = item.children().find(|e| e.has_tag_name("Key"))?;
 			let key_str = key.text()?;
